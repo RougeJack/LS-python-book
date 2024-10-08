f663d43c41563624eb3605f13b25d5071e734dce
Brian Jagiel <brianjagiel@live.com>
2024-10-08 08:15:49 -0400

    First exercise in variable chapter

---
+++ variables/exercise-1.py
@@ -0,0 +1,10 @@
+# 1. Classify the following potenital non-constant variable names as idomatic, non-idiomatic, or illegal. For the non-idiomatic and illegal names, explain your choice.
+
+index           |           idiomatic
+CatName         |           idiomatic
+lazy_dog        |           idiomatic
+quick_Fox       |           non-idiomatic   |   Used uppercase letter in Fox
+1stCharacter    |           illegal         |   Starts with a number
+operand2        |           idiomatic   
+BIG_NUMBER      |           non-idiomatic   |   Uppercase letters
+ π              |           non-idiomatic   |   Not an ASCII character
