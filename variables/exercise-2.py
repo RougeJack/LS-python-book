e2794147788aaeaea3bf3e2ec8ebd48a509ad36e
Brian Jagiel <brianjagiel@live.com>
2024-10-08 08:23:06 -0400

    Add second exercise in variable chapter

---
+++ variables/exercise-2.py
@@ -0,0 +1,10 @@
+# Classify the following potential function names as idiomatic, non-idiomatic, or illegal.
+
+index           |           idiomatic
+CatName         |           non-idiomatic           |   Uppercase letter
+lazy_dog        |           idiomatic
+quick_Fox       |           non-idiomatic           |   Uppercase letter
+1stCharacter    |           illegal                 |   starts with number
+operand2        |           idiomatic
+BIG_NUMBER      |           non-idiomatic           |   Uppercase letters
+π               |           non-idiomatic           |   π not an ASCII character
