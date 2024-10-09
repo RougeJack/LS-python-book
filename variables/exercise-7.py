7d661049fd9c749c7899bf06eed49beb1e4faa0b
Brian Jagiel <brianjagiel@live.com>
2024-10-09 08:25:48 -0400

    added rest of exercises in variable chapter

---
+++ variables/exercise-7.py
@@ -0,0 +1,6 @@
+# What happens when you run the following code? Why?
+
+When the code is ran it first prints 'Victor' three times.
+Then 'Nina' is printed three times.
+Names are stored as variables but meant to be constants.  Python,
+doesn't have true constants.  
