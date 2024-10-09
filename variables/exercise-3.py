5b64b93085b5a47db1fa72347b2479026d2d5eba
Brian Jagiel <brianjagiel@live.com>
2024-10-09 07:34:44 -0400

    Added exercise 3 from variables chapter

---
+++ variables/exercise-3.py
@@ -0,0 +1,10 @@
+# Classify the following potential constant names as idiomatic, non-idiomatic, or illegal.
+
+index           |           non-idiomatic       |       should not use lowercase letters
+CatName         |           non-idiomatic       |       should not use lowercase letters
+snake_case      |           non-idiomatic       |       should not use lowercase letters
+LAZY_DOG3       |           idiomatic
+1ST             |           illegal             |       should not begin with a number
+operand2        |           non-idiomatic       |       should not use lowercase letters
+BIG_NUMBER      |           idiomatic    
+Π               |           non-idiomatic       |       Π is not a ASCII character
