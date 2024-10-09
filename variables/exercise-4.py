d93f7f68347e3f923322a1896da09208694f1529
Brian Jagiel <brianjagiel@live.com>
2024-10-09 07:40:10 -0400

    Added exercise 4 from variables chapter

---
+++ variables/exercise-4.py
@@ -0,0 +1,9 @@
+# Classify the following potential class names as idiomatic, non-idiomatic, or illegal.  
+
+index           |       non-idiomatic       |       should not begin with a lowercase letter
+CatName         |       idiomatic
+Lazy_Dog        |       non-idiomatic       |       should not use underscores
+1st             |       illegal             |       starts with a number
+operand2        |       non-idiomatic       |       starts with a lowercase letter
+BigNumber3      |       idiomatic           
+Πi              |       non-idiomatic       |       not a ASCII character
